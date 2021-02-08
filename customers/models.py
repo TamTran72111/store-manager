13689dd44ae0b1d76434ca5e3762400bbfbd4671
TamTran72111 <tam.tran72111@gmail.com>
2021-02-07 20:57:17 -0500

    Add search feature for customer

--- customers/models.py
+++ customers/models.py
@@ -1,8 +1,12 @@
 from django.db import models
+from django.db.models.signals import pre_save
+
+from utils.mixins import SearchNameMixin
 
 
-class Customer(models.Model):
+class Customer(models.Model, SearchNameMixin):
     name = models.CharField(max_length=200)
+    search_name = models.CharField(max_length=200, blank=True)
     description = models.TextField(blank=True)
     phone = models.CharField(max_length=20, blank=True)
     address = models.CharField(max_length=256, blank=True)
@@ -10,3 +14,6 @@ class Customer(models.Model):
 
     def __str__(self):
         return self.name
+
+
+pre_save.connect(Customer.pre_save, Customer, dispatch_uid='Customer_pre_save')
